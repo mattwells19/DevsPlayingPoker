a3e26dd46e7cf7087f66ec83698f9489084f1563
Josh Bastean <joshua.bastean@levvel.io>
2022-07-20 16:09:58 -0400

    Increase possible room code letters

--- server/utils/generateRoomCode.ts
+++ server/utils/generateRoomCode.ts
@@ -16,8 +16,7 @@ const generateRoomCode = async (): Promise<string> => {
 
 const getRandomCode = () => {
   let text = '';
-  // const possible = 'ABCDEFGHIJKLMNOPQRSTUVWXYZ';
-  const possible = 'AB'; // Reduced roomCode options for testing
+  const possible = 'ABCDEFGHIJKLMNOPQRSTUVWXYZ';
   for (let i = 0; i < 4; i++) {
     text += possible.charAt(Math.floor(Math.random() * possible.length));
   }
