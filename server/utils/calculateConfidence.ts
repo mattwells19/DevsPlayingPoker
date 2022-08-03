0fe0cde3b2671c1402ab0b627299f374b45df815
Sara Hunter <34786124+sarashunter@users.noreply.github.com>
2022-08-03 08:49:50 -0400

    add optionselected event and confidence calculator (#32)
    * add optionselected event and confidence calculator
    
    * switch confidence times
    
    * use same function to update moderator and voters
    
    * update jsdoc for sendroomdata
    
    * move error throw into sendroomdata
    
    * appease typescript gods
    
    Co-authored-by: Sara Hunter <sara.hunter@endava.com>

---
+++ server/utils/calculateConfidence.ts
@@ -0,0 +1,22 @@
+import { ConfidenceValue } from "../models/room.model.ts";
+
+const calculateConfidence = (timeStarted: Date | null): ConfidenceValue => {
+	if (!timeStarted) {
+		throw new Error(
+			`Unable to calculate voting confidence.  No timeStarted found.`,
+		);
+	}
+	const currentTime = new Date();
+	const millisecondsElapsed = currentTime.getTime() - timeStarted.getTime();
+	const secondsElapsed = millisecondsElapsed / 1000;
+
+	if (secondsElapsed < 5) {
+		return ConfidenceValue.high;
+	}
+	if (secondsElapsed < 15) {
+		return ConfidenceValue.medium;
+	}
+	return ConfidenceValue.low;
+};
+
+export default calculateConfidence;
