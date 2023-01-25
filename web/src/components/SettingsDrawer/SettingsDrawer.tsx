965124612d44b7498196bb5fdf96b20a0cafc7f0
Matt <matt.wells@endava.com>
2023-01-25 16:56:38 -0500

    Try to fix drawer on mobile

--- web/src/components/SettingsDrawer/SettingsDrawer.tsx
+++ web/src/components/SettingsDrawer/SettingsDrawer.tsx
@@ -7,6 +7,7 @@ import {
 	ParentComponent,
 	Show,
 } from "solid-js";
+import { Portal } from "solid-js/web";
 import Button from "../Button";
 import styles from "./SettingsDrawer.module.scss";
 
@@ -56,77 +57,77 @@ const SettingsDrawer: ParentComponent<SettingsDrawerProps> = (props) => {
 	};
 
 	return (
-		<div
-			class={styles.backdrop}
-			classList={{ [styles.open]: props.isOpen }}
-			onClick={props.onClose}
-		>
-			<aside class={styles.drawer} onClick={(e) => e.stopPropagation()}>
-				<Button
-					variant="ghost"
-					onClick={props.onClose}
-					class={styles.closeBtn}
-					title={intl.t("closeSettingsDrawer") as string}
-				>
-					&#10005;
-				</Button>
-				<div class={styles.themeSelect}>
-					<label for="theme-select">{intl.t("theme")}</label>
-					<select
-						id="theme-select"
-						name="theme-select"
-						aria-describedby="theme-select-helptext"
-						value={localStorage.getItem("theme") ?? "system"}
-						onChange={handleThemeChange}
-					>
-						<option value="system">{intl.t("system")}</option>
-						<option value="light">{intl.t("light")}</option>
-						<option value="dark">{intl.t("dark")}</option>
-					</select>
-					<p id="theme-select-helptext">{intl.t("savesAutomatically")}</p>
-				</div>
-				<Show when={props.onSaveName}>
-					<form
-						class={styles.nameForm}
-						onSubmit={(e) => {
-							e.preventDefault();
-							const formData = new FormData(e.currentTarget);
-							const name = formData.get("name")?.toString().trim();
+		<Portal>
+			<Show when={props.isOpen}>
+				<div class={styles.backdrop} onClick={props.onClose}>
+					<aside class={styles.drawer} onClick={(e) => e.stopPropagation()}>
+						<Button
+							variant="ghost"
+							onClick={props.onClose}
+							class={styles.closeBtn}
+							title={intl.t("closeSettingsDrawer") as string}
+						>
+							&#10005;
+						</Button>
+						<div class={styles.themeSelect}>
+							<label for="theme-select">{intl.t("theme")}</label>
+							<select
+								id="theme-select"
+								name="theme-select"
+								aria-describedby="theme-select-helptext"
+								value={localStorage.getItem("theme") ?? "system"}
+								onChange={handleThemeChange}
+							>
+								<option value="system">{intl.t("system")}</option>
+								<option value="light">{intl.t("light")}</option>
+								<option value="dark">{intl.t("dark")}</option>
+							</select>
+							<p id="theme-select-helptext">{intl.t("savesAutomatically")}</p>
+						</div>
+						<Show when={props.onSaveName}>
+							<form
+								class={styles.nameForm}
+								onSubmit={(e) => {
+									e.preventDefault();
+									const formData = new FormData(e.currentTarget);
+									const name = formData.get("name")?.toString().trim();
 
-							if (!name || name.length === 0) {
-								setErrorMsg("enterAName");
-								return;
-							}
-							if (name.length > 10) {
-								setErrorMsg("nameTooLong");
-								return;
-							}
+									if (!name || name.length === 0) {
+										setErrorMsg("enterAName");
+										return;
+									}
+									if (name.length > 10) {
+										setErrorMsg("nameTooLong");
+										return;
+									}
 
-							props.onSaveName!(name);
-						}}
-					>
-						<label for="name">{intl.t("name")}</label>
-						<input
-							id="name"
-							name="name"
-							type="text"
-							required
-							minLength="1"
-							maxLength="10"
-							autofocus
-							value={localStorage.getItem("name") ?? ""}
-							onInput={() => setErrorMsg(null)}
-							aria-describedby="name-error-msg"
-							aria-invalid={Boolean(errorMsg())}
-						/>
-						<Show when={errorMsg()} keyed>
-							{(msg) => <p id="name-error-msg">{intl.t(msg)}</p>}
+									props.onSaveName!(name);
+								}}
+							>
+								<label for="name">{intl.t("name")}</label>
+								<input
+									id="name"
+									name="name"
+									type="text"
+									required
+									minLength="1"
+									maxLength="10"
+									autofocus
+									value={localStorage.getItem("name") ?? ""}
+									onInput={() => setErrorMsg(null)}
+									aria-describedby="name-error-msg"
+									aria-invalid={Boolean(errorMsg())}
+								/>
+								<Show when={errorMsg()} keyed>
+									{(msg) => <p id="name-error-msg">{intl.t(msg)}</p>}
+								</Show>
+								<Button type="submit">{intl.t("saveName")}</Button>
+							</form>
 						</Show>
-						<Button type="submit">{intl.t("saveName")}</Button>
-					</form>
-				</Show>
-			</aside>
-		</div>
+					</aside>
+				</div>
+			</Show>
+		</Portal>
 	);
 };
 
